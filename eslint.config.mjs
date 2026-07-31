586f05d49c19f6193d95906704817c674f572c70
Joan Robles <rpjoandavid@gmail.com>
2026-07-30 22:56:51 -0500

    fix: exclude Next generated types from lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,3 @@
 import { FlatCompat } from '@eslint/eslintrc';
 const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
-export default [...compat.extends('next/core-web-vitals', 'next/typescript'), { ignores: ['.next/**','node_modules/**'] }];
+export default [...compat.extends('next/core-web-vitals', 'next/typescript'), { ignores: ['.next/**','node_modules/**','next-env.d.ts'] }];
